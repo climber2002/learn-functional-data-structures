6d28874f5f69c5a2fc03b43be59dfcf78bdeb568
Andy Wang <climber2002@gmail.com>
2019-06-15 12:37:57 +1000

    Add preorder, inorder, postorder

--- src/main/scala/com/functional/ch03/Tree.scala
+++ src/main/scala/com/functional/ch03/Tree.scala
@@ -14,4 +14,52 @@ object BinTree {
       Branch(x, buildTree(xs.take(k)), buildTree(xs.drop(k)))
     }
   }
+
+  def size[A](tree: BinTree[A]): Int = tree match {
+    case Leaf => 0
+    case Branch(_, left, right) => 1 + size(left) + size(right)
+  }
+
+  def depth[A](tree: BinTree[A]): Int = tree match {
+    case Leaf => 0
+    case Branch(_, left, right) => 1 + (depth(left) max depth(right))
+  }
+
+  def buildCompleteTree(v: Int, depth: Int): BinTree[Int] =
+    if(depth == 0) Leaf
+    else Branch(v, buildCompleteTree(2 * v, depth - 1), buildCompleteTree(2 * v + 1, depth - 1))
+
+  def equal[A](tree1: BinTree[A], tree2: BinTree[A]): Boolean = (tree1, tree2) match {
+    case (Leaf, Leaf) => true
+    case (Leaf, Branch(_, _, _)) => false
+    case (Branch(_, _, _), Leaf) => false
+    case (Branch(v1, l1, r1), Branch(v2, l2, r2)) => v1 == v2 && equal(l1, l2) && equal(r1, r2)
+  }
+
+  def flip[A](tree: BinTree[A]): BinTree[A] = tree match {
+    case Leaf => Leaf
+    case Branch(v, l, r) => Branch(v, flip(r), flip(l))
+  }
+
+  def flippedEqual[A](tree1: BinTree[A], tree2: BinTree[A]): Boolean = (tree1, tree2) match {
+    case (Leaf, Leaf) => true
+    case (Branch(v1, l1, r1), Branch(v2, l2, r2)) if v1 == v2 =>
+      flippedEqual(l1, r2) && flippedEqual(l2, r1)
+    case _ => false
+  }
+
+  def preorder[A](tree: BinTree[A]): List[A] = tree match {
+    case Leaf => Nil
+    case Branch(v, l, r) => v :: preorder(l) ++ preorder(r)
+  }
+
+  def inorder[A](tree: BinTree[A]): List[A] = tree match {
+    case Leaf => Nil
+    case Branch(v, l, r) => inorder(l) ++ (v :: inorder(r))
+  }
+
+  def postorder[A](tree: BinTree[A]): List[A] = tree match {
+    case Leaf => Nil
+    case Branch(v, l, r) => postorder(l) ++ postorder(r) ++ List(v)
+  }
 }
